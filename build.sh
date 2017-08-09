9773dab8c7afed4e049f434e72dedef726191ffe
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-08-09 13:49:16 +0200

    Update build.sh to adhere to GSON library changes.

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@
 echo "Start building Postcode Modules..."
 rm -rf bin
 mkdir bin
-javac -d . --module-source-path src $(find src -name "*.java")
+javac -p nl.ekholabs.address.checker:com.google.gson -d nl.ekholabs.postcode.validator $(find src/nl.ekholabs.postcode.validator -name "*.java")
+javac -p com.google.gson -d com.google.gson $(find src/com.google.gson -name "*.java")
+javac -p nl.ekholabs.postcode.validator:com.google.gson -d nl.ekholabs.address.checker $(find src/nl.ekholabs.address.checker -name "*.java")
+
+echo "Creating the GSON library JAR module"
+jar --create --file bin/gson.jar --module-version=1.0 -C com.google.gson .
 
 echo "Creating the Postcode Validator JAR module..."
 jar --create --file bin/postcodevalidator.jar --module-version=1.0 -C nl.ekholabs.postcode.validator .
@@ -13,7 +18,7 @@ jar --create --file bin/addresschecker.jar --module-version=1.0 --main-class=nl.
 
 echo "Creating compressed Java Image..."
 rm -rf linkedmod
-jlink --module-path $JAVA_HOME/jmods:bin --add-modules nl.ekholabs.address.checker --output linkedmod --compress=2
+jlink -p $JAVA_HOME/jmods:bin --add-modules nl.ekholabs.address.checker --output linkedmod --compress=2
 
 echo "Done!"
 echo "To run the application, please type: linkedmod/bin/java -m nl.ekholabs.address.checker 3452RG nl"
